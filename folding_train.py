087cb44f4424f530444723f3215061d35dfaa8fb
jgvictores <jcgvicto@gmail.com>
2025-01-26 11:34:41 +0100

    train on correct data

--- folding_train.py
+++ folding_train.py
@@ -13,6 +13,7 @@ NUM_TRAIN_BATCHES = 2 # NUM TRAIN_IMAGES = BATCH_SIZE * NUM_TRAIN_BATCHES
 NUM_TEST_BATCHES = 1 # NUM TEST_IMAGES = BATCH_SIZE * NUM_TEST_BATCHES
 
 labels = np.loadtxt(path.join(DATASET_DIR,"labels.txt"), delimiter=',', usecols=range(1,5))
+#print("labels",labels)
 
 def define_model():
     model = models.Sequential([
@@ -34,12 +35,13 @@ for idx_outer in range(NUM_TRAIN_BATCHES):
     list_of_arrays = []
     for idx_inner in range(BATCH_SIZE):
         image_name = path.join(DATASET_DIR,"image"+str(idx_outer*BATCH_SIZE+idx_inner)+".png")
-        print("** Loading", image_name)
+        print("** x_train["+str(idx_inner)+"]", image_name)
         array = utils.img_to_array(Image.open(image_name))
         list_of_arrays.append(array)
-    X_train = tf.stack(list_of_arrays)
-    y_train = np.random.rand(BATCH_SIZE, 4)  # Example target values
-    model.fit(X_train, y_train, epochs=10, batch_size=BATCH_SIZE)
+    x_train = tf.stack(list_of_arrays)
+    y_train = labels[idx_outer*BATCH_SIZE:(idx_outer+1)*BATCH_SIZE]
+    print("** y_train", y_train)
+    model.fit(x_train, y_train, epochs=10, batch_size=BATCH_SIZE)
     print("* end batch",idx_outer)
 
 # Example prediction
