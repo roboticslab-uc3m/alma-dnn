bc8533d124133ad93d86c91b6b5101429175bf29
jgvictores <jcgvicto@gmail.com>
2025-01-26 11:17:08 +0100

    better batch parameter naming

--- folding_train.py
+++ folding_train.py
@@ -8,7 +8,8 @@ from PIL import Image
 IMAGE_SIZE = (300, 300, 3)
 IMAGE_DIR = "dataset"
 BATCH_SIZE = 16
-BATCHES = 2 # NUM IMAGES = BATCH_SIZE * BATCHES
+NUM_TRAIN_BATCHES = 2 # NUM TRAIN_IMAGES = BATCH_SIZE * NUM_TRAIN_BATCHES
+NUM_TEST_BATCHES = 1 # NUM TEST_IMAGES = BATCH_SIZE * NUM_TEST_BATCHES
 
 def define_model():
     model = models.Sequential([
@@ -25,8 +26,8 @@ def define_model():
 model = define_model()
 
 # Train the model
-for idx_outer in range(2):
-    print("* begin",idx_outer)
+for idx_outer in range(NUM_TRAIN_BATCHES):
+    print("* begin batch",idx_outer)
     list_of_arrays = []
     for idx_inner in range(BATCH_SIZE):
         image_name = path.join(IMAGE_DIR,"image"+str(idx_outer*BATCH_SIZE+idx_inner)+".png")
@@ -36,10 +37,9 @@ for idx_outer in range(2):
     X_train = tf.stack(list_of_arrays)
     y_train = np.random.rand(BATCH_SIZE, 4)  # Example target values
     model.fit(X_train, y_train, epochs=10, batch_size=BATCH_SIZE)
-    print("* end",idx_outer)
+    print("* end batch",idx_outer)
 
 # Example prediction
-# X_test should be your test images
-X_test = np.random.rand(10, IMAGE_SIZE[0], IMAGE_SIZE[1], IMAGE_SIZE[2])  # Example test data
+X_test = np.random.rand(NUM_TEST_BATCHES*BATCH_SIZE, IMAGE_SIZE[0], IMAGE_SIZE[1], IMAGE_SIZE[2])  # Example test data
 predictions = model.predict(X_test)
 print(predictions)
