eab6b42ff8fc5bd7d4fe9918998ebfc974aecb44
jgvictores <jcgvicto@gmail.com>
2025-02-04 10:49:39 +0100

    folding_vis.py draw circles

--- folding_vis.py
+++ folding_vis.py
@@ -5,6 +5,10 @@ import sys
 DATASET_DIR = "dataset"
 
 IMAGE_SIZE = (100, 100)
+pick_color = "red"
+pick_radius = 5
+place_color = "green"
+place_radius = 5
 
 if len(sys.argv) < 5:
     print("Usage:  folding_vis.py image_name pick_x pick_y place_x place_y")
@@ -12,18 +16,20 @@ if len(sys.argv) < 5:
     quit(1)
 
 image_name = sys.argv[-5]
-pick_x = sys.argv[-4]
-pick_y = sys.argv[-3]
-place_x = sys.argv[-2]
-place_y = sys.argv[-1]
+pick_x = round(float(sys.argv[-4]))
+pick_y = round(float(sys.argv[-3]))
+place_x = round(float(sys.argv[-2]))
+place_y = round(float(sys.argv[-1]))
 
 pygame.init()
  
 scrn = pygame.display.set_mode(IMAGE_SIZE)
  
-imp = pygame.image.load(image_name).convert()
+surf1 = pygame.image.load(image_name).convert()
+pygame.draw.circle(surf1, pick_color, (pick_x,pick_y), pick_radius)
+pygame.draw.circle(surf1, place_color, (place_x,place_y), place_radius)
  
-scrn.blit(imp, (0, 0))
+scrn.blit(surf1, (0, 0))
  
 pygame.display.flip()
 status = True
